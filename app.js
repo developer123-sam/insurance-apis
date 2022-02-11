0febb9ca4a551bb4433d29330dd3413047d7c344
Shadab1node <node1flyweis@gmail.com>
2022-02-11 18:31:41 +0530

    solved

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 let express = require("express");
-var path=require("path")
+var path = require("path")
 let bodyParser = require("body-parser");
 require("./Config/database")
 const twilio = require('twilio');
@@ -11,27 +11,28 @@ app.use(
     origin: "*",
   })
 );
+app.use("/upload", express.static(path.join(__dirname, "upload")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 let signRoutes = require("./routes/signin-routes");
 let vehicalRoutes = require("./routes/vehical-routes");
 let fuelRoutes = require("./routes/fuel-routes");
-let serviceRoutes=require("./routes/service-routes")
-let stationRoutes=require("./routes/servicestation-routes")
-let notificationRoutes=require("./routes/notification-route")
-let adminRoutes=require("./routes/admin-routes")
-let referalRoutes=require("./routes/referal-routes")
+let serviceRoutes = require("./routes/service-routes")
+let stationRoutes = require("./routes/servicestation-routes")
+let notificationRoutes = require("./routes/notification-route")
+let adminRoutes = require("./routes/admin-routes")
+let referalRoutes = require("./routes/referal-routes")
 
 var port = process.env.PORT || 2999;
 app.use("/api",
-adminRoutes,
-signRoutes,
-fuelRoutes,
-vehicalRoutes,
-serviceRoutes,
-stationRoutes,
-notificationRoutes,
-referalRoutes
+  adminRoutes,
+  signRoutes,
+  fuelRoutes,
+  vehicalRoutes,
+  serviceRoutes,
+  stationRoutes,
+  notificationRoutes,
+  referalRoutes
 )
 app.listen(port, function () {
   console.log("Running on port " + port);
