100a787f09b8a55fe27b4dad282f8401c9891fc8
Shadab1node <node1flyweis@gmail.com>
2022-02-18 14:02:51 +0530

    push

--- controller/shareController.js
+++ controller/shareController.js
@@ -17,10 +17,14 @@ exports.share = async (req, res) => {
 
 // GET-SHARE-POST
 
-// exports.getshare = async (req, res) => {
-//     try {
-//         const findvehicle = await Vehicle.find({})
-//     } catch (error) {
-
-//     }
-// }
+exports.getshare = async (req, res) => {
+    try {
+        const share = new Share(req.body);
+        share.shareby = req.user._id
+        share.save()
+        return res.status(200).json({ msg: "share successfullly" })
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ msg: "something went wrong" })
+    }
+}
